8103d413653edbe071dd11ac48361bf7350f2029
asdrubal <asdrubaloviedo@gmail.com>
2024-10-04 11:09:06 -0500

    swagger fixes

--- swagger-res/index.js
+++ swagger-res/index.js
@@ -5,15 +5,14 @@ const {
 const express = require('express');
 const serverless = require('serverless-http');
 const swaggerUI = require('swagger-ui-express');
-const { TextDecoder } = require('util'); // Para decodificar el Uint8Array
+const { TextDecoder } = require('util');
 
 const apigateway = new APIGatewayClient({});
-
 const app = express();
 
 // Define valores fijos para apiId y stage
-const apiId = 'tarpy1226l'; // Reemplaza con tu API ID real
-const stage = 'dev'; // Reemplaza con tu stage real
+const apiId = 'tarpy1226l';
+const stage = 'dev';
 
 module.exports.handler = async (event, context) => {
   const params = {
@@ -27,36 +26,44 @@ module.exports.handler = async (event, context) => {
     const command = new GetExportCommand(params);
     const getExportPromise = await apigateway.send(command);
 
-    // Log del response crudo para depuración
-    console.log('Raw response:', getExportPromise.body);
-    console.log('Response type:', typeof getExportPromise.body);
+    // Verifica si el tipo de contenido es 'application/octet-stream'
+    if (getExportPromise.contentType === 'application/octet-stream') {
+      const responseBody = new TextDecoder('utf-8').decode(
+        getExportPromise.body
+      );
 
-    // Convierte Uint8Array a string
-    const responseBody = new TextDecoder('utf-8').decode(getExportPromise.body);
+      // Intenta parsear el JSON desde la respuesta
+      let swaggerJson;
+      try {
+        swaggerJson = JSON.parse(responseBody);
+      } catch (jsonError) {
+        console.error('Failed to parse JSON:', jsonError);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            error: 'Invalid JSON response from API Gateway'
+          })
+        };
+      }
 
-    // Intenta parsear la respuesta JSON
-    let swaggerJson;
-    try {
-      swaggerJson = JSON.parse(responseBody);
-    } catch (jsonError) {
-      console.error('Failed to parse JSON:', jsonError);
+      // Eliminar rutas no deseadas de Swagger JSON
+      delete swaggerJson.paths['/api-docs/{proxy+}'];
+      delete swaggerJson.paths['/api-docs'];
+
+      // Sirve Swagger UI con el JSON válido de Swagger
+      app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
+      const handler = serverless(app);
+      const ret = await handler(event, context);
+      return ret;
+    } else {
+      console.error('Unexpected content type:', getExportPromise.contentType);
       return {
         statusCode: 500,
         body: JSON.stringify({
-          error: 'Invalid JSON response from API Gateway'
+          error: 'Unexpected content type from API Gateway'
         })
       };
     }
-
-    // Eliminar rutas no deseadas de Swagger JSON
-    delete swaggerJson.paths['/api-docs/{proxy+}'];
-    delete swaggerJson.paths['/api-docs'];
-
-    // Sirve Swagger UI con el JSON válido de Swagger
-    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
-    const handler = serverless(app);
-    const ret = await handler(event, context);
-    return ret;
   } catch (error) {
     console.error('Error retrieving API Gateway export:', error);
     return {
